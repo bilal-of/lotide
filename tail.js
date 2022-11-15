160102e55a280284861319ac965e72429fd8a69c
Bilal <bfarah@ualberta.ca>
2022-11-15 02:47:19 +0000

    completed tail code

---
+++ tail.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const tail = function(arr) {
+  arr.shift();
+  return arr;
+
+};
+console.log(tail(words));
