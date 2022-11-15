986e380821d2e0a7c9926fdf4257bfd188b4cd18
Bilal <bfarah@ualberta.ca>
2022-11-15 02:07:59 +0000

    implement head code

--- head.js
+++ head.js
@@ -10,4 +10,11 @@ const assertEqual = function(actual, expected) {
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(1, 1); 
+
+const head = function(arr) { 
+  return arr[0]
+} 
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
