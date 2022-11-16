a79f1e7aa86a4b107d44f195a298868ea4df6e07
Bilal <bfarah@ualberta.ca>
2022-11-16 01:31:05 +0000

    created assertion for previous file eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) { 
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } 
+  else { 
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const eqArrays = function(array1, array2) { 
+  for (i = 0; i < array1.length; i++) { 
+    if (array1[i] !== array2[i]) {
+      return false
+    } 
+  }  
+  return true
+     
+
+}; 
+
+const assertArraysEqual = function (arr1, arr2) { 
+  if (eqArrays(arr1, arr2)) { 
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  } 
+  else { 
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
+
+  }
+}
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
